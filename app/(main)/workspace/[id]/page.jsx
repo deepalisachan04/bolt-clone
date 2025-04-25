7ab5300cddb2decceb9970ab9f908244b44170b5
Ash-wika <2k22.cse.2211647@gmail.com>
2025-04-25 16:10:08 +0530

    Changes done in original code.... @React + @ReactDom download and convex ka package download kra tha to resolve the error. * api krke tha use {api} likha tha console me jaake client and children related error sort

---
+++ app/(main)/workspace/[id]/page.jsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import ChatView from "@/components/custom/ChatView";
+import CodeView from "@/components/custom/CodeView";
+
+function Workspace() {
+  return (
+    <div className="p-10">
+      <div className="grid grid-cols-1 md:grid-cols-4">
+        <ChatView />
+        <div className="col-span-3">
+          <CodeView />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Workspace;
